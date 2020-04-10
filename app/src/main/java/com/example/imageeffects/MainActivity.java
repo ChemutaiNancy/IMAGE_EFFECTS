f457c9695f2b539a65c9c8d3758dd3b2ea82d392
Nancy <chemutain@gmail.com>
2020-04-10 15:01:45 +0300

    image inversion

--- app/src/main/java/com/example/imageeffects/MainActivity.java
+++ app/src/main/java/com/example/imageeffects/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.imageeffects;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView myimageview;
+    Drawable myfruit;
+    Bitmap bitmapImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myimageview = findViewById(R.id.imageView);
+        myfruit = ResourcesCompat.getDrawable(getResources(), R.drawable.apple, null);//import an image
+        bitmapImage = ((BitmapDrawable) myfruit).getBitmap();
+        Bitmap newphoto = invertImage(bitmapImage);
+        myimageview.setImageBitmap(newphoto);
     }
 }
